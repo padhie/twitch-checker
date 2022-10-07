7be5b6e5ba60db455d70a545a58965aa8d4b90e4
Padhie <padhie@users.noreply.github.com>
2022-10-07 22:13:02 +0200

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,18 @@
 <?php
 
-return [
+$packages = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
+
+if (file_exists(__DIR__ . '/bundles_dev.php')) {
+    $packages = array_merge(
+        $packages,
+        include(__DIR__ . '/bundles_dev.php')
+    );
+}
+
+return $packages;
